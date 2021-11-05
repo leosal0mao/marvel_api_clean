9773255b6912efe533c553683d1de42f6d272bd7
Leo Salomao <leonardo.salomao@crmall.com>
2021-11-05 18:07:06 -0300

    endpoints and modular config

---
+++ lib/app/core/helpers/endpoints/endpoints.dart
@@ -0,0 +1,9 @@
+class Endpoints {
+  static const timestamp = '1';
+  static const apiKey = 'b915b6bb75332f81594e5b17ca1fed32';
+  static const hash = 'eb8ba09a1868c49634afd0fa5f1089b4';
+
+  static const baseUrl = 'https://gateway.marvel.com/v1/public';
+  static const charactersUrl =
+      '/characters?ts=$timestamp&apikey=$apiKey&hash=$hash';
+}
